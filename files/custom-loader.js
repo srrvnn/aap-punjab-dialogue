d9de475605962b00e32c33026b06b1b049a65a7b
Manav Agarwal <manav.agarwal4@gmail.com>
2014-10-24 01:20:03 +0200

    loader error still not fixed

--- files/custom-loader.js
+++ files/custom-loader.js
@@ -1,6 +1,17 @@
 jQuery(document).ready(function(){
  
-
+jQuery("body").queryLoader2({
+            onComplete: function() {},
+            onLoadComplete: function() {},
+            backgroundColor: "#fff",
+            barColor: dt_loader.bcolor,
+            overlayId: 'qLoverlay',
+            barHeight: 4,
+            percentage: false,
+            deepSearch: true,
+            completeAnimation: "fade",
+            minimumTime: 500    
+        });
 ready = 1;
 
 });
