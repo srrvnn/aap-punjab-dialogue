3a617428cd62de47bea8ce6e614ec6d2122e4906
Manav Agarwal <manav.agarwal4@gmail.com>
2014-10-24 01:17:04 +0200

    Loader fixed and vimal errors fixed

--- files/custom-loader.js
+++ files/custom-loader.js
@@ -1,18 +1,6 @@
 jQuery(document).ready(function(){
  
-        jQuery("body").queryLoader2({
-            onComplete: function() {},
-            onLoadComplete: function() {},
-            backgroundColor: "#fff",
-            barColor: dt_loader.bcolor,
-            overlayId: 'qLoverlay',
-            barHeight: 4,
-            percentage: false,
-            deepSearch: true,
-            completeAnimation: "fade",
-            minimumTime: 500    
-        });
 
-        jQuery("#qLoverlay").css({'display':'none'});
+ready = 1;
 
 });
