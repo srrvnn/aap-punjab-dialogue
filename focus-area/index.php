7c2ddbce127dc4254fec749502f17b5a6b7ba7ae
Saravanan Ganesh <saravanan.ganesh@ensighten.com>
2016-04-23 20:28:34 -0700

    google sheets poc

--- focus-area/index.php
+++ focus-area/index.php
@@ -53,6 +53,70 @@
 </head>
 <body>
 
+	<script>
+
+	// script to retreive what I am learning currently and display it on the page
+	var request = new XMLHttpRequest();
+	request.open('GET', 'https://spreadsheets.google.com/feeds/list/19w9sl1c9HuxkUeRkT7ThCQSfD-mjGTrm6FJE2B4hn6k/od6/public/values?alt=json', true);
+
+	request.onload = function() {
+
+	  if (request.status >= 200 && request.status < 400) {
+
+	    // Success!
+	    var data = JSON.parse(request.responseText);
+	    var logs = data.feed.entry;
+	    var elLogs = document.createElement('div');
+
+	    console.dir(data);
+
+	    // iterate over links
+
+	    // for (var i = logs.length-1; i >= 0; i--) {
+	      // var elLog = document.createElement('div');
+
+	      // var log = logs[i];
+
+	      // var logLink = log.gsx$link.$t;
+	      // var logTitle = log.gsx$title.$t;
+	      // var logTime = String(log.gsx$date.$t).replace(/ at(.*)/, '');
+
+	      // create anchor tags
+
+	      // var aLink = document.createElement('a');
+	      // aLink.href = logLink;
+	      // aLink.innerHTML = logTitle;
+	      // aLink.className = 'mild';
+
+	      // var spanTime = document.createElement('span');
+	      // spanTime.innerHTML = logTime;
+
+	      // append them on the page
+
+	      // elLog.className = 'log';
+	      // elLog.appendChild(aLink);
+	      // elLog.appendChild(spanTime);
+
+	      // elLogs.appendChild(elLog);
+	    // }
+
+	    // document.querySelector('#logs').innerHTML = elLogs.innerHTML;
+
+	  } else {
+
+	  	// something went wrong
+	  }
+	};
+
+	request.onerror = function() {
+
+	  // There was a connection error of some sort
+	};
+
+	request.send();
+
+	</script>
+
 	<!-- Following script is for Google Analytics -->
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
