165db70ebf642742a1f1b9782ecbbd9340f4109e
Saravanan Ganesh <saravanan.ganesh@ensighten.com>
2016-05-26 07:58:02 -0700

    contact us fix: use cc, and new body

--- services/contact-us.php
+++ services/contact-us.php
@@ -14,14 +14,14 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com;';                     // Specify main and backup SMTP servers
 $mail->SMTPDebug   = 2;                               // 2 to enable SMTP debug information
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = "punjabdialoguefeedback@gmail.com";   // SMTP username
-$mail->Password = "punjabdialoguefeedback123";                   // SMTP password
+$mail->Username = "punjabdialoguecontact@gmail.com";   // SMTP username
+$mail->Password = "punjabdialogue";                   // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
 $mail->setFrom('punjabdialoguecontact@gmail.com', 'Arvind Kejriwal (On Behalf of the Punjab Dialogue Team)');
 $mail->addAddress('srrvnn@live.com');               // Name is optional
-// $mail->addCC('srrvnn@live.com');
+$mail->addCC('srrvnn@gmail.com');
 // $mail->addCC('aappunjabdialogue@gmail.com');
 // $mail->addReplyTo('info@example.com', 'Information');
 // $mail->addBCC('bcc@example.com');
@@ -30,9 +30,14 @@ $mail->addAddress('srrvnn@live.com');               // Name is optional
 // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+$mail->Subject = 'Welcome to Punjab Dialogue';
+$mail->Body = "Dear Saravanan,\n\n Thank you for writing to the team at Delhi Dialogue. We appreciate your feedback and inputs. The strong secretariat at Delhi Dialogue will have a look at your suggestions and surely keep them in mind whilst thinking of Delhi's future.
+\nDelhi Dialogue is an initiative of the Aam Aadmi Party that presents a concrete, sustainable and futuristic roadmap to make Delhi a world-class city. It seeks to conduct a comprehensive study of the intractable problems that plague Delhi and take stock of the aspirations of its citizens. Invitations will be sent to domain experts, researchers, scientists, national and international experts from governments,  organizations, businesses and civil society to engage with us on a 30-point agenda themed under 12 focus areas, whilst catering to the needs, wants and desires of Delhi's youth, women, traders, middle class living in group housing societies, RWAs, JJ Clusters, other housing colonies and villages. This will be then used to create a concrete, feasible and long-term roadmap and vision to make Delhi a smart but liveable city.
+\nFeel free to write in to us at delhidialogue@aamaadmiparty.org or whatsapp us at +919643327265 or visit our website to track progress at http://delhidialogue.in/index.html
+\nYou can also follow us on Twitter > https://twitter.com/DelhiDialogue or Facebook > https://www.facebook.com/pages/Delhi-Dialogue/576718279124066?ref=br_tf as well!
+\n We look forward to continuing this process of Dialogue with you. Keep in touch!
+\nYours sincerely,\nArvind Kejriwal\n(On Behalf of the Delhi Dialogue Team)";
+$mail->AltBody = '';
 
 if(!$mail->send()) {
     error_log("did not send");
