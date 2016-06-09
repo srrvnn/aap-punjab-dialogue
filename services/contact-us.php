59aa0346d6fae5195d394787aa6cdd383277104d
Saravanan Ganesh <saravanan.ganesh@ensighten.com>
2016-06-08 19:37:47 -0700

    updates to contact us info

--- services/contact-us.php
+++ services/contact-us.php
@@ -3,36 +3,28 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 class ContactUsResponse {
-  public $status = "InvalidParameters";
-  public $message = "Required parameters are not passed or empty";
-	public $messageId = "2";
+    public $status = "InvalidParameters";
+    public $message = "Required parameters are not passed or empty";
+    public $messageId = "2";
 }
 
 $mail = new PHPMailer;
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com;';                     // Specify main and backup SMTP servers
-// $mail->SMTPDebug   = 2;                               // 2 to enable SMTP debug information
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = "punjabdialoguecontact@gmail.com";   // SMTP username
-$mail->Password = "punjabdialogue";                   // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+$mail->isSMTP();                                       // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com;';                       // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                                // Enable SMTP authentication
+$mail->Username = "punjabdialoguefeedback@gmail.com";   // SMTP username
+$mail->Password = "punjabdialoguefeedback123";                    // SMTP password
+$mail->SMTPSecure = 'tls';                             // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                     // TCP port to connect to
+$mail->isHTML(true);                                   // Set email format to HTML
 
-$mail->setFrom('punjabdialoguecontact@gmail.com', 'Arvind Kejriwal (On Behalf of the Punjab Dialogue Team)');
-$mail->addCC('srrvnn@live.com');
+$mail->setFrom('punjabdialoguefeedback@gmail.com', 'Arvind Kejriwal (On Behalf of the Punjab Dialogue Team)');
+$mail->addReplyTo('aappunjabdialogue@gmail.com', 'AAP Punjab Dialogue');
 // $mail->addCC('aappunjabdialogue@gmail.com');
-// $mail->addReplyTo('info@example.com', 'Information');
-// $mail->addBCC('bcc@example.com');
-
-// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$secret = '6LfDHR4TAAAAAEO7WlESvnf4_5Vdccwfirg514Yw';
+$mail->addBCC('srrvnn@live.com');
 
 $response = new ContactUsResponse();
-// $is_debug = isset($_GET['q121_aap_dd_debug_counter']);
 
 // Your code here to handle a successful verification
 // Checking for blank Fields..
@@ -45,7 +37,7 @@ if (!isset($_POST["name"]) || empty($_POST["name"]) || !isset($_POST["email"]) |
     $name = $_POST['name'];
     $message = $_POST['message'];
 
-	  // Check if the "Sender's Email" input field is filled out
+	// Check if the "Sender's Email" input field is filled out
     $email = $_POST['email'];
 
     // Sanitize e-mail address
@@ -57,52 +49,49 @@ if (!isset($_POST["name"]) || empty($_POST["name"]) || !isset($_POST["email"]) |
     if (!$email) {
 
       $response->status = "ValidationError";
-		  $response->message = "Email is invalid!";
+      $response->message = "Email is invalid!";
 
       echo json_encode($response);
 
     } else {
 
-      $recaptcha = new \ReCaptcha\ReCaptcha($secret);
-		  $capchaResponse = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+        $recaptcha = new \ReCaptcha\ReCaptcha('6LfDHR4TAAAAAEO7WlESvnf4_5Vdccwfirg514Yw');
+        $capchaResponse = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
 
-		// if ($is_debug) {
+        if (!$capchaResponse->isSuccess()) {
 
-		// 	echo "verify_capcha_status = ".var_dump($capchaResponse);
-		// }
+            echo json_encode($capchaResponse);
+            return;
+        }
 
-		if (!$capchaResponse->isSuccess()) {
+        // Send mail by PHP Mail Function
+        $mail->addAddress("$email", "$name");
 
-			echo json_encode($capchaResponse);
-			return;
-		}
+        $mail->Subject = 'Welcome to Punjab Dialogue';
+        $mail->Body = "Dear $name,\n\n Thank you for writing to the team at Punjab Dialogue. We appreciate your feedback and inputs. The strong secretariat at Punjab Dialogue will have a look at your suggestions and surely keep them in mind whilst thinking of Punjab's future.
 
-    // Send mail by PHP Mail Function
-    $mail->addAddress("$email", "$name");
+            \nPunjab Dialogue is an initiative of the Aam Aadmi Party that presents a concrete, sustainable and futuristic roadmap to make Punjab a world-class state. It seeks to conduct a comprehensive study of the intractable problems that plague Punjab and take stock of the aspirations of its citizens. Invitations will be sent to domain experts, researchers, scientists, national and international experts from governments,  organizations, businesses and civil society to engage with us on a 30-point agenda themed under 12 focus areas, whilst catering to the needs, wants and desires of Punjab's youth, women, traders, middle class living in group housing societies, RWAs, JJ Clusters, other housing colonies and villages. This will be then used to create a concrete, feasible and long-term roadmap and vision to make Punjab a smart and liveable state.
+            \nFeel free to write in to us at aappunjabdialogue@gmail.com or WhatsApp us at +919643327265 or visit our website to track progress at http://punjabdialogue.org
+            \nYou can also follow us on Twitter > https://twitter.com/pbdialogue or Facebook > https://www.facebook.com/punjabdialogue as well!
+            \n We look forward to continuing this process of Dialogue with you. Keep in touch!
+            \nYours sincerely,\nArvind Kejriwal\n(On Behalf of the Punjab Dialogue Team) <hr/> $message";
 
-    $mail->Subject = 'Welcome to Punjab Dialogue';
-    $mail->Body = "Dear $name,\n\n Thank you for writing to the team at Delhi Dialogue. We appreciate your feedback and inputs. The strong secretariat at Delhi Dialogue will have a look at your suggestions and surely keep them in mind whilst thinking of Delhi's future.
-    \nDelhi Dialogue is an initiative of the Aam Aadmi Party that presents a concrete, sustainable and futuristic roadmap to make Delhi a world-class city. It seeks to conduct a comprehensive study of the intractable problems that plague Delhi and take stock of the aspirations of its citizens. Invitations will be sent to domain experts, researchers, scientists, national and international experts from governments,  organizations, businesses and civil society to engage with us on a 30-point agenda themed under 12 focus areas, whilst catering to the needs, wants and desires of Delhi's youth, women, traders, middle class living in group housing societies, RWAs, JJ Clusters, other housing colonies and villages. This will be then used to create a concrete, feasible and long-term roadmap and vision to make Delhi a smart but liveable city.
-    \nFeel free to write in to us at delhidialogue@aamaadmiparty.org or whatsapp us at +919643327265 or visit our website to track progress at http://delhidialogue.in/index.html
-    \nYou can also follow us on Twitter > https://twitter.com/DelhiDialogue or Facebook > https://www.facebook.com/pages/Delhi-Dialogue/576718279124066?ref=br_tf as well!
-    \n We look forward to continuing this process of Dialogue with you. Keep in touch!
-    \nYours sincerely,\nArvind Kejriwal\n(On Behalf of the Delhi Dialogue Team)";
-    $mail->AltBody = '';
+        $mail->AltBody = '';
 
-    if(!$mail->send()) {
+        if (!$mail->send()) {
 
-      $response->message = "Mail could not be sent!";
-      $response->messageId = "10";
-      $response->status = "MailingError";
+            $response->message = "Mail could not be sent!";
+            $response->messageId = "10";
+            $response->status = "MailingError";
 
-    } else {
+        } else {
 
-      $response->message = "Email was sent successfully!";
-      $response->messageId = "3";
-      $response->status = "Completed";
-    }
+            $response->message = "Email was sent successfully!";
+            $response->messageId = "3";
+            $response->status = "Completed";
+        }
 
-    echo json_encode($response);
+        echo json_encode($response);
     }
 }
 
